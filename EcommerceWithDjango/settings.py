a95ee1a80b796cc84505467b3ec8761e1ec22d20
Saad Ahmad <20800995+saadcse35@users.noreply.github.com>
2020-07-25 12:02:07 +0600

    25th July 2020

--- EcommerceWithDjango/settings.py
+++ EcommerceWithDjango/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'EcommerceWithDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT': '3306',
+      }
+  }
+
 
 
 # Password validation
